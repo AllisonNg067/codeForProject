e994912576020de8fd8435c599bcf7f9640840bb
AllisonNg067 <95010322+AllisonNg067@users.noreply.github.com>
2022-02-02 11:01:27 +0800

    Update final_qwoa_maxcut.py

--- final_qwoa_maxcut.py
+++ final_qwoa_maxcut.py
@@ -2,15 +2,11 @@ import mpi4py.MPI
 import h5py
 from quop_mpi.algorithm import qwoa
 from quop_mpi import observable
-from quop_mpi.toolkit import I, Z
-import networkx as nx
 from numpy import random
-Graph = nx.circular_ladder_graph(4)
 
-vertices = len(Graph.nodes)
+vertices = l6
 system_size = 2 ** vertices
 
-G = nx.to_scipy_sparse_matrix(Graph)
 random.seed(1)
 #Modify it 
 def maxcut_qualities(n_qubits):
@@ -24,4 +20,4 @@ alg = qwoa(system_size)
 alg.set_qualities(observable.serial, {"function": maxcut_qualities, "args": [vertices]})
 alg.set_log("final_qwoa_maxcut.csv", "final_qwoa_maxcut", action="w")
 alg.set_depth(2)
-alg.benchmark(range(1, 20), 3, param_persist=True, filename="qwoa_results", save_action="w")
+alg.benchmark(range(1, 6), 3, param_persist=True, filename="qwoa_results", save_action="w")
